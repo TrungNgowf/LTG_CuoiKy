eaa0e7e88c803dcd7a0c9546ab2239a0b28e3fbc
TrungNg2502 <94361113+TrungNg2502@users.noreply.github.com>
2023-12-08 23:45:22 +0700

    trungnv commit

--- Assets/Scripts/Players/CharactersController.cs
+++ Assets/Scripts/Players/CharactersController.cs
@@ -6,11 +6,13 @@ public class CharactersController : MonoBehaviour
 {
     public GameObject huntress;
     public GameObject godOfWind;
+    public GameObject godOfCrystal;
     // Start is called before the first frame update
     void Start()
     {
-         huntress.SetActive(true);
+        huntress.SetActive(true);
         godOfWind.SetActive(false);
+        godOfCrystal.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,12 +23,18 @@ public class CharactersController : MonoBehaviour
             huntress.SetActive(false);
             godOfWind.SetActive(true);
             godOfWind.transform.localPosition = huntress.transform.localPosition;
-        } ;
+        };
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             huntress.SetActive(true);
             godOfWind.SetActive(false);
             huntress.transform.localPosition = godOfWind.transform.localPosition;
+        };
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            huntress.SetActive(false);
+            godOfCrystal.SetActive(true);
+            godOfCrystal.transform.localPosition = huntress.transform.localPosition;
         };
     }
 }
